afdfc0b34095bf3cb61e9637717ab163f7dd7d0c
LunarSera <29496284+LunarSera@users.noreply.github.com>
2018-12-09 14:50:01 -0500

    bug fixes

--- cogs/owner.py
+++ cogs/owner.py
@@ -48,9 +48,42 @@ class Owner:
         self.disabled_commands = dataIO.load_json("data/red/disabled_commands.json")
         self.global_ignores = dataIO.load_json("data/red/global_ignores.json")
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
+        self.owner = self.get_owner()
 
     def __unload(self):
         self.session.close()
+
+    def get_owner(self):
+        owner_id = dataIO.load_json("data/red/settings.json")["OWNER"]
+        return discord.utils.find(lambda m: m.id == owner_id, self.bot.get_all_members())
+
+    async def send_to_owner(self, **kwargs):
+        if self.owner is None:
+            self.owner = self.get_owner()
+        await self.bot.send_message(self.owner, **kwargs)
+
+    async def on_message(self, message: discord.Message):
+        if self.owner is None:
+            self.owner = self.get_owner()
+        if not message.channel.is_private or message.channel.user.id == self.owner.id:
+            return
+
+        embed = discord.Embed()
+        if message.author == self.bot.user:
+            embed.title = 'Sent PM to {}#{} ({}).'.format(message.channel.user.name, message.channel.user.discriminator, message.channel.user.id)
+        else:
+            embed.set_author(name=message.author, icon_url=message.author.avatar_url or message.author.default_avatar_url)
+            embed.title = '{} messaged me:'.format(message.channel.user.id)
+        embed.description = message.content
+        embed.timestamp = message.timestamp
+
+        await self.send_to_owner(embed=embed)
+
+    @commands.command()
+    @checks.is_owner()
+    async def pm(self, user: discord.User, *, content: str):
+        """PMs a person."""
+        await self.bot.send_message(user, content)
 
     @commands.command()
     @checks.is_owner()
